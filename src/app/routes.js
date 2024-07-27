35956879704a8e51e779308028c2e578f993b911
A.Mahmoudi <rminmh@gmail.com>
2024-07-27 10:28:32 +0330

    DEVELOP => add cost model

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,8 +3,9 @@ import { Router } from 'express';
 //____________________* Entities Routes*____________________//
 import user from './user/user.route.js';
 import worktime from './worktime/worktime.route.js';
+import cost from './cost/cost.route.js';
 
-const apiRoutes = Router().use(user, worktime);
+const apiRoutes = Router().use(user, worktime, cost);
 
 const routes = Router().use('/api', apiRoutes);
 
