fc920128272b133331e9beaeadc3d955e9ef73dc
A.Mahmoudi <rminmh@gmail.com>
2024-07-27 11:10:08 +0330

    DEVELOP => add user/register model

--- src/app/user/user.controller.js
+++ src/app/user/user.controller.js
@@ -33,4 +33,59 @@ export class User {
 			});
 		});
 	};
+
+	static register = async (req, res, next) => {
+		const username = req?.params?.username || '';
+		const password = req?.body?.password;
+		const securityCode = req?.body?.securityCode;
+
+		if (securityCode !== 118303)
+			return ServerResponse.json(res, {
+				success: false,
+				message: 'invalid code',
+				body: { info: null },
+			});
+
+		if (!username || !password)
+			return ServerResponse.json(res, {
+				success: false,
+				message: 'invalid parameters',
+				body: { info: null },
+			});
+
+		const records = JSON.parse(FS.readFileSync(__filename_db_user)?.data || '{}');
+
+		const findedRecord = records?.[username] || null;
+
+		if (findedRecord)
+			return ServerResponse.json(res, {
+				success: false,
+				message: 'user is registered',
+				body: { info: null },
+			});
+
+		const record = {
+			id: Date.now(),
+			username: username ?? null,
+			password: password ?? null,
+			lastName: null,
+			firstName: null,
+		};
+
+		const { data, err } = FS.writeFileSync(__filename_db_user, JSON.stringify({ ...records, [username]: record }));
+
+		if (err)
+			return ServerResponse.json(res, {
+				success: false,
+				message: 'can not register',
+				body: { info: null },
+			});
+
+		delete record['password'];
+		ServerResponse.json(res, {
+			success: true,
+			message: 'success',
+			body: { info: record },
+		});
+	};
 }
--- src/app/user/user.route.js
+++ src/app/user/user.route.js
@@ -5,5 +5,6 @@ import { User } from './user.controller.js';
 const router = express.Router();
 
 router.post('/v1/users/:username/login', User.login);
+router.post('/v1/users/:username/register', User.register);
 
 export default router;
