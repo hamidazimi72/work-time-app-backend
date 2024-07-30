0208e8d434a1910fa005938f32c225d4526718a1
A.Mahmoudi <rminmh@gmail.com>
2024-07-30 13:48:13 +0330

    DEVELOP =>  mirror

--- src/app/user/user.controller.js
+++ src/app/user/user.controller.js
@@ -115,4 +115,73 @@ export class User {
 			body: { info: record, token: token },
 		});
 	};
+
+	static resetPassword = async (req, res, next) => {
+		const username = req?.user?.username;
+
+		if (!username)
+			return ServerResponse.json(res, {
+				success: false,
+				statusCode: 401,
+				message: 'user not found',
+				body: { info: null },
+			});
+
+		const password = req?.body?.password;
+		const newPassword = req?.body?.newPassword;
+
+		if (!password || !newPassword)
+			return ServerResponse.json(res, {
+				success: false,
+				message: 'invalid parameters',
+				body: { info: null },
+			});
+
+		const records = User.functions.readAllRecords() || {};
+
+		const findedRecord = records[username] || null;
+
+		if (!findedRecord)
+			return ServerResponse.json(res, {
+				success: false,
+				message: 'user is not registered',
+				body: { info: null },
+			});
+
+		const isValidPassword = bcrypt.compareSync(password, findedRecord.password);
+
+		if (!isValidPassword)
+			return ServerResponse.json(res, {
+				success: false,
+				message: 'password not correct',
+				body: { info: null },
+			});
+
+		const hashedPassword = bcrypt.hashSync(newPassword, 10);
+
+		const record = {
+			...findedRecord,
+			password: hashedPassword,
+		};
+
+		const { data, err } = FS.writeFileSync(
+			__filename_db_user,
+			JSON.stringify({ ...records, [username]: record }, null, 2)
+		);
+
+		if (err)
+			return ServerResponse.json(res, {
+				success: false,
+				message: 'can not change password',
+				body: { info: null },
+			});
+
+		delete record['password'];
+
+		ServerResponse.json(res, {
+			success: true,
+			message: 'success',
+			body: { info: record },
+		});
+	};
 }
--- src/app/user/user.route.js
+++ src/app/user/user.route.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/v1/users/:username/login', User.login);
 router.post('/v1/users/:username/register', User.register);
+router.post('/v1/users/:username/reset-password', User.resetPassword);
 
 export default router;
