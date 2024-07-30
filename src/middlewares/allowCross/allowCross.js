79cd2158a3d854042494f88b4565ecfe501a40d6
A.Mahmoudi <rminmh@gmail.com>
2024-07-30 07:53:08 +0330

    DEVELOP =>  check cors error

--- src/middlewares/allowCross/allowCross.js
+++ src/middlewares/allowCross/allowCross.js
@@ -2,8 +2,9 @@ export const allowCross = (req, res, next) => {
 	if (typeof res?.setHeader !== 'function' || typeof next !== 'function') return;
 
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+	res.setHeader('Access-Control-Allow-headers', '*');
+	// res.setHeader('Access-Control-Allow-Credentials', 'true');
 
 	next();
 };
